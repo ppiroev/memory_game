8112d7032c6472d92f99dac2948dd8b0d0ecafb6
ppiroev <p.piroev@abv.bg>
2023-05-19 11:41:18 +0300

    update img src for github pages

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,67 +1,67 @@
 const cards = [
   {
     name: "5 AM Uber",
-    src: "./src/images/5-am-uber.jpg",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/5-am-uber.jpg",
   },
   {
     name: "Adventure",
-    src: "./src/images/adventure.jpg",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/adventure.jpg",
   },
   {
     name: "Monday Morning",
-    src: "./src/images/after-party.jpg",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/after-party.jpg",
   },
   {
     name: "Agent Rick",
-    src: "./src/images/agent-rick.png",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/agent-rick.png",
   },
   {
     name: "Commando Rick",
-    src: "./src/images/commando-rick.png",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/commando-rick.png",
   },
   {
     name: "Drink-n-Drive",
-    src: "./src/images/drink-n-drive.jpg",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/drink-n-drive.jpg",
   },
   {
     name: "Eu-Rick-a",
-    src: "./src/images/euricka.jpg",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/euricka.jpg",
   },
   {
     name: "Eyes Open",
-    src: "./src/images/eyes-open.png",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/eyes-open.png",
   },
   {
     name: "Gangsta Morty",
-    src: "./src/images/gangsta-morty.jpg",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/gangsta-morty.jpg",
   },
   {
     name: "Horatio Rick",
-    src: "./src/images/horatio-rick.png",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/horatio-rick.png",
   },
   {
     name: "Oh Shit!",
-    src: "./src/images/ohshit.jpg",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/ohshit.jpg",
   },
   {
     name: "Oooops!",
-    src: "./src/images/ops.jpg",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/ops.jpg",
   },
   {
     name: "Portal Jump",
-    src: "./src/images/portal.jpg",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/portal.jpg",
   },
   {
     name: "Ripped Rick",
-    src: "./src/images/ripped-rick.jpg",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/ripped-rick.jpg",
   },
   {
-    name: "Split in pieces",
-    src: "./src/images/split-in-pieces.jpg",
+    name: "Gemini",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/split-in-pieces.jpg",
   },
   {
     name: "Terminator Rex",
-    src: "./src/images/terminator.jpg",
+    src: "https://raw.githubusercontent.com/ppiroev/memory_game/main/src/images/terminator.jpg",
   },
 ];
 
